ee471951bbba88d033f2ef1b24330fdf48844489
Siddharth Jangid <jangidsiddharth@gmail.com>
2025-05-23 14:26:20 +0530

    Remove unused things

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -34,8 +34,6 @@ const Dashboard = () => {
     }
   }, [selectedCategory, products]);
 
-  const handleCategory = (e: any) => {};
-
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
